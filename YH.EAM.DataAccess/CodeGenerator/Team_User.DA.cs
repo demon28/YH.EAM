fa63b8e947f33b33072cd1f8b6f31f19c2fc3f5b
demon28 <274733956@qq.com>
2020-07-21 17:53:13 +0800

    提交

---
+++ YH.EAM.DataAccess/CodeGenerator/Team_User.DA.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using YH.EAM.DataAccess;
+using YH.EAM.Entity.CodeGenerator;
+
+namespace YH.EAM.DataAccess.CodeGenerator
+{
+
+    /// <summary>
+    ///  
+    ///</summary>
+    public class Team_User_Da : DbBaseDal<Team_User>
+    {
+        
+         
+
+
+    }
+
+}
+
