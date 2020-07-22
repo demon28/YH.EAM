210867c4c7a4aaef17c94d825a966d0fd99f4060
demon28 <274733956@qq.com>
2020-07-23 01:30:29 +0800

    提交

--- YH.EAM.DataAccess/CodeGenerator/Team_user.DA.cs
+++ YH.EAM.DataAccess/CodeGenerator/Team_user.DA.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
+using Victory.Core.Models;
 using YH.EAM.DataAccess;
 using YH.EAM.Entity.CodeGenerator;
+using YH.EAM.Entity.Enums;
+using YH.EAM.Entity.Model;
 
 namespace YH.EAM.DataAccess.CodeGenerator
 {
@@ -11,10 +14,16 @@ namespace YH.EAM.DataAccess.CodeGenerator
     /// <summary>
     ///  
     ///</summary>
-    public class Team_User_Da : DbBaseDal<Team_User>
+    public class Team_User_Da : FreeSql.BaseRepository<Team_User>
     {
-        
-         
+
+        public Team_User_Da() : base(DataAccess.DbContext.Db, null, null)
+        {
+
+
+        }
+
+
 
 
     }
