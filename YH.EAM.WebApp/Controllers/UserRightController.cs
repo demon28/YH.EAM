51356291330989e62b50fbc951218b0e035dddef
demon28 <274733956@qq.com>
2020-07-22 17:40:00 +0800

    差权限

---
+++ YH.EAM.WebApp/Controllers/UserRightController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YH.EAM.WebApp.Controllers
+{
+    public class UserRightController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult NoPermission() {
+
+            return View();
+
+        }
+
+
+
+       
+        public IActionResult Role()
+        {
+            return View();
+        }
+
+        public IActionResult Func()
+        {
+            return View();
+        }
+    }
+}
